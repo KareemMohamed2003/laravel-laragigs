aebe5ebb9282db95a2862a6c761ee19d0de44a26
kareemMohamed2003 <hagershawkat@gmail.com>
2023-09-17 17:20:02 +0300

    added images files , changed layout file

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,23 +1,58 @@
 @extends ('layout')
- {{-- by using the extends directive we are telling the listing.blade.php file
+{{-- by using the extends directive we are telling the listing.blade.php file
      that it will be a child template of the  layout template --}}
 
 @section('content')
-<h1> {{$heading}}</h1>
-@unless (count($listings)==0)
+    {{-- <h1> {{ $heading }}</h1> --}}
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
-@foreach ($listings as $listing)
+        @unless (count($listings) == 0)
+            @foreach ($listings as $listing)
+                <div class="bg-gray-50 border border-gray-200 rounded p-6">
+                    <div class="flex">
+                        <img class="hidden w-48 mr-6 md:block" src="images/acme.png" alt="" />
+                        <div>
+                            <h3 class="text-2xl">
+                                <a href="show.html">{{$listing->title}}</a>
+                            </h3>
+                            <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
+                            <ul class="flex">
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                    <a href="#">Laravel</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                    <a href="#">API</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                    <a href="#">Backend</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                    <a href="#">Vue</a>
+                                </li>
+                            </ul>
+                            <div class="text-lg mt-4">
+                                <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
+
+                {{--
 <h2>
 <a href="/listings/{{$listing["id"]}}">
-{{-- {{$listing}} --}}
+
 {{$listing['title']}}
 </a>
 </h2>
 <p>
  {{$listing['description']}}
-</p>
-@endforeach
-@endunless
+</p> --}}
+            @endforeach
+        @endunless
+    </div>
 @endsection
-
